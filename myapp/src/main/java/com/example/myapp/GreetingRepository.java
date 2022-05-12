b5f0271d997b5aadb722e97a43810377b0bcae9b
Jonathan Mark Allison <jallison@nexient.com>
2022-05-11 22:20:25 -0400

    initial

---
+++ myapp/src/main/java/com/example/myapp/GreetingRepository.java
@@ -0,0 +1,7 @@
+package myapp;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface GreetingRepository extends CrudRepository<Greeting, Long> {
+    // for now only using default CRUD methods
+}   
